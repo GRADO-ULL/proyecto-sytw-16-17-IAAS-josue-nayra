3574be64954e09b96f43826a3a81e1b1c05d9ffa
josuetc94 <alu0100763492@ull.edu.es>
2016-10-19 16:41:31 +0000

    Actualizando plugin para despliegue en iaas.

--- gitbook-start-iaas-ull-es-josue-nayra.js
+++ gitbook-start-iaas-ull-es-josue-nayra.js
@@ -1,11 +1,26 @@
 "use strict"
 
+const exec = require('child_process').exec;
+
 console.log("File gitbook-start-iaas-ull-es.js");
-var deploy = ((ip_maquina,source) => 
+
+var respuesta = ((error, stdout, stderr) => 
+{
+    if (error)
+        console.error("Error:"+error);
+    console.log("Stderr:"+stderr);
+    console.log("Stdout:"+stdout);
+});
+
+var deploy = ((ip_maquina,source,url) => 
 {
     console.log("Realizando deploy...");
     console.log("Ip_maquina:"+ip_maquina);
     console.log("Source:"+source);
+    console.log("Url:"+url);
+    
+    // exec(`ssh ${ip_maquina}; cd ${source}; git clone ${url} master`, respuesta);
+    exec(`ssh ${ip_maquina}; cd ${source}; git pull ${url} master`, respuesta);
 });
 
 exports.deploy = deploy;
