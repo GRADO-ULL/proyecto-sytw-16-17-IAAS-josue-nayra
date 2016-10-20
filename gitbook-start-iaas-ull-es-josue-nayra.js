99363ebcd7dd80669b1aace476002d95d392fa15
josuetc94 <alu0100763492@ull.edu.es>
2016-10-20 10:38:23 +0000

    Actualizando plugin para despliegue en iaas.

--- gitbook-start-iaas-ull-es-josue-nayra.js
+++ gitbook-start-iaas-ull-es-josue-nayra.js
@@ -1,10 +1,9 @@
 "use strict"
 const exec = require('ssh-exec');
-// const exec = require('child_process').exec;
 
-console.log("File gitbook-start-iaas-ull-es.js");
+// console.log("File gitbook-start-iaas-ull-es.js");
 
-var respuesta = ((error, stdout, stderr) => 
+var respuesta = ((error, stdout, stderr) =>
 {
     if (error)
         console.error("Error:"+error);
@@ -12,24 +11,18 @@ var respuesta = ((error, stdout, stderr) =>
     console.log("Stdout:"+stdout);
 });
 
-var deploy = ((ip_maquina,source,url,usuario,password) => 
+var deploy = ((ip_maquina,source,url,usuario) =>
 {
     console.log("Realizando deploy...");
     console.log("Ip_maquina:"+ip_maquina);
     console.log("Source:"+source);
     console.log("Url:"+url);
-    
-    // exec(`ssh ${ip_maquina}; cd ${source}; git clone ${url} master`, respuesta);
-    // exec(`ssh ${ip_maquina}; cd ${source}; git pull ${url} master`, respuesta);
-    exec(`cd ${source}; git pull ${url} master`, {
-       user: `${usuario}`,
-       host: `${ip_maquina}`,
-       password: `${password}`
-    },function(err, stdout, stderr){
-        console.log("Error: "+err);
-        console.log("Salida: "+stdout);
-        console.log("Stderr: "+stderr);
-    });
+
+    exec('cd src/sytw/migitbook/; git pull origin master', {
+      user: 'usuario',
+      host: '10.6.128.176',
+      key: '~/.ssh/id_rsa.pub'
+    }, respuesta);
 });
 
 exports.deploy = deploy;
