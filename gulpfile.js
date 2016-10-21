a4e141bda0bb8e0e16fefaac9dedf5342472ebd0
josuetc94 <alu0100763492@ull.edu.es>
2016-10-21 10:35:30 +0000

    Actualizando plugin para despliegue en iaas.

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,16 @@ var gulp = require('gulp');
 var git = require('simple-git');
 var install = require('gulp-install');
 var shell = require('gulp-shell');
+var myArgs = require('minimist')(process.argv.slice(2));
 
 //---------------------------------------------------------------------------
 // Tarea para subir al repositorio.
 
 gulp.task('push', function(){
-   git()
+    var mensaje_commit = myArgs.mensaje || "Actualizando plugin para despliegue en iaas.";
+    git()
         .add('./*')
-        .commit("Actualizando plugin para despliegue en iaas.")
+        .commit(mensaje_commit)
         .push('origin','master');
 });
 
