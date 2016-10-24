c19b5aaecbf3f4795659ebc8d7aa2161917b410d
josuetc94 <alu0100763492@ull.edu.es>
2016-10-24 20:09:25 +0000

    Actualizando plugin para despliegue en iaas.

--- gitbook-start-iaas-ull-es-josue-nayra.js
+++ gitbook-start-iaas-ull-es-josue-nayra.js
@@ -1,9 +1,11 @@
 "use strict"
 const exec = require('ssh-exec');
-const exec_shell = require('exec');
+// const exec_shell = require('exec');
 const basePath = process.cwd();
 const fs = require('fs-extra');
 const path = require('path');
+var exec_shell = require('child_process').exec;
+
 // console.log("File gitbook-start-iaas-ull-es.js");
 
 var respuesta = ((error, stdout, stderr) =>
@@ -29,18 +31,36 @@ var deploy = ((ip_maquina,source,url,usuario) =>
 });
 
 var initialize = ((ip_maquina,usuario,source, url) => {
-    
+
     console.log("Método initialize del plugin deploy-iaas-ull-es");
-    
-    var tarea_gulp = `\n\ngulp.task("deploy-iaas-ull-es", function(){`+
+    //
+    var tarea_gulp = `\n\ngulp.task("deploy-iaas-ull-es", ["deploy"], function(){`+
              `\n       require("gitbook-start-iaas-ull-es-josue-nayra").deploy("${ip_maquina}", "${source}", "${url}");`+
-             `\n});`;     
-    
-    fs.appendFile(path.join(basePath,'gulpfile.js'), `${tarea_gulp}`, (err) => {
-      if (err) throw err;
-        console.log("Escribiendo tarea en gulpfile para próximos despliegues");        
+             `\n});`;
+
+    fs.readFile('gulpfile.js', "utf8", function(err, data) {
+        if (err) throw err;
+        // console.log(data);
+        if(data.search("deploy-iaas-ull-es") != -1)
+        {
+          console.log("Ya existe una tarea de deploy-iaas-ull-es");
+        }
+        else
+        {
+          // console.log("No existe una tarea de deploy-iaas-ull-es");
+          fs.appendFile(path.join(basePath,'gulpfile.js'), `${tarea_gulp}`, (err) => {
+            if (err) throw err;
+              console.log("Escribiendo tarea en gulpfile para próximos despliegues");
+          });
+        }
     });
-    
+
+    // exec(`cd ~/.ssh/; cp iaas.pub authorized_keys`, {
+    //   user: usuario,
+    //   host: ip_maquina,
+    //   key: '~/.ssh/id_rsa.pub'
+    // }, respuesta);
+
     //Creamos ficheros de clave pública
     // exec_shell(['cd ~/.ssh/; ssh-keygen -f iaas'], function(err, out, code) {
     //   if (err instanceof Error)
@@ -61,7 +81,7 @@ var initialize = ((ip_maquina,usuario,source, url) => {
     //       user: usuario,
     //       host: ip_maquina,
     //       key: '~/.ssh/id_rsa.pub'
-    //     }, respuesta);    
+    //     }, respuesta);
     // });
 });
 
